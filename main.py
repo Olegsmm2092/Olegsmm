f8cef66b42f37e58fd75a2a5fb0a93215a1d5042
Olegsmm2092 <radoveev@gmail.com>
2023-10-25 11:36:23 +0800

    add new func to calc to divide

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from calc import add
+from calc import add, divide
 from greeting import greet
 
 
 def main():
    greet()
-   output = add(5,5)
-   print(f'calc operation {add.__name__} = {output}')
+   operation = str(input('type a calc operation (add/divide): '))
+   # print(operation is add.__name__) # bag
+   # print(operation ==  add.__name__) # fix bag
+   if operation is add.__name__:
+      output = add(5,5)
+      print(f'calc operation {add.__name__} = {output}')
+   elif operation is divide.__name__:
+      output = divide(5,5)
+      print(f'calc operation {divide.__name__} = {output}')
+   else:
+      raise Exception(operation)
 
 if __name__ == '__main__':
    main()
